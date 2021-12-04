67af5784a5ea26811cdecb9424cd186654f89bce
hanabzu <hanthird@gmail.com>
2021-12-04 20:46:15 +0900

    Create test2

---
+++ test2.c
@@ -0,0 +1,89 @@
+// test code
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/list.h> // gclist.h
+#include <linux/slab.h>
+#include <linux/ktime.h>
+#include <linux/kthread.h>
+#include <linux/delay.h>
+#include "wlist.h"
+
+
+#define COUNT 10 // node 100 개
+int test_insert_and_search(void* data);
+void test_delete(void);
+
+struct task_struct* writer_thread1, * writer_thread2, * writer_thread3, * writer_thread4;
+
+struct my_node {
+	struct wlist_head whead;
+	int data;
+};
+
+struct wlist_head my_list; //head
+struct my_node *current_node = NULL;
+struct my_node *tmp = NULL;
+
+int __init mod_init(void){
+	INIT_GCLIST_HEAD(&my_list); // initialize list head
+	int i;
+
+	/*먼저 100개 노드를 insert해 둔다. */
+	
+	for (i = 0; i < COUNT; i++){
+		struct my_node *new = kmalloc(sizeof(struct my_node),GFP_KERNEL);
+		new->data = i;
+		wlist_add(&new->head, &my_list);
+	}	
+	writer_thread1 = kthread_run(test_insert_and_search, NULL, "test_insert_and_search");
+	writer_thread2 = kthread_run(test_insert_and_search, NULL, "test_insert_and_search");
+	writer_thread3 = kthread_run(test_insert_and_search, NULL, "test_insert_and_search");
+	writer_thread4 = kthread_run(test_insert_and_search, NULL, "test_insert_and_search");
+	printk("hello module! \n");
+	return 0;
+}
+
+void __exit mod_cleanup(void) {
+	printk("bye module \n");
+}
+my_node->whead->head->next;
+module_init(mod_init);
+module_exit(mod_cleanup);
+MODULE_LICENSE("GPL");
+
+int test_insert_and_search(void* data)
+{
+	// search 
+	
+	int i;
+	for (i = 0; i < COUNT; i++){
+		wlist_for_each_entry_safe(current_node,tmp, &my_list, head ){
+			if(current_node->data == i){
+				//printk("current node->data: %d \n", current_node->data);
+				struct my_node *new = kmalloc(sizeof(struct my_node), GFP_KERNEL);
+				new->data = i*1000;
+				wlist_add(&new->head, &(current_node->head));
+			}
+		}
+	}	
+	ssleep(1);
+	// 잘 삽입되었는지 확인000000000000000000000000000000
+	printk("================result=================\n");
+	wlist_for_each_entry_safe(current_node, tmp, &my_list, head){
+		printk("current node-> data : %d \n", current_node->data);
+	
+	}
+	return 0;
+}
+
+void test_delete(void){
+	
+	gclist_for_each_entry_safe(current_node, tmp, &my_list, head){
+		if (current_node->data == 2){
+			printk("current node value :%d \n", current_node->data);
+			gclist_del(&current_node->list);
+			kfree(current_node);
+		}
+	}
+	
+}
