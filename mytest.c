aba5d263d06bd367b7c2f0b95ac5a58a1dfb0eaf
freemjstudio <mjwoo0@naver.com>
2021-12-04 22:07:09 +0900

    test.c

--- mytest.c
+++ mytest.c
@@ -10,47 +10,35 @@
 
 #define COUNT 10 // node 100 개
 int test_insert_and_search(void* data);
-int test_delete(void* data);
-
-unsigned long long start_insert;
-unsigned long long end_insert;
-unsigned long long start_search;
-unsigned long long end_search;
-unsigned long long start_delete;
-unsigned long long end_delete;
+void test_delete(void);
 
 struct task_struct* writer_thread1, * writer_thread2, * writer_thread3, * writer_thread4;
 
 struct my_node {
-	struct list_head list;
+	struct gclist_head list;
 	int data;
 	 
 };
 
-struct list_head my_list; //head
+struct gclist_head my_list; //head
 	struct my_node *current_node = NULL;
 	struct my_node *tmp = NULL;
 
 int __init mod_init(void){
-	INIT_LIST_HEAD(&my_list); // initialize list head
-		int i;
+	INIT_GCLIST_HEAD(&my_list); // initialize list head
+	int i;
 
 	/*먼저 100개 노드를 insert해 둔다. */
 	
 	for (i = 0; i < COUNT; i++){
 		struct my_node *new = kmalloc(sizeof(struct my_node),GFP_KERNEL);
 		new->data = i;
-		list_add(&new->list, &my_list);
+		gclist_add(&new->list, &my_list);
 	}	
-
-	// writer_thread1 = kthread_run(test_insert_and_search, NULL, "test_insert_and_search");
-	// writer_thread2 = kthread_run(test_insert_and_search, NULL, "test_insert_and_search");
-	// writer_thread3 = kthread_run(test_insert_and_search, NULL, "test_insert_and_search");
-	// writer_thread4 = kthread_run(test_insert_and_search, NULL, "test_insert_and_search");
-
-	writer_thread1 = kthread_run(test_delete, NULL, "test_delete");
-	writer_thread2 = kthread_run(test_delete, NULL, "test_delete");
-	writer_thread3 = kthread_run(test_delete, NULL, "test_delete");
+	writer_thread1 = kthread_run(test_insert_and_search, NULL, "test_insert_and_search");
+	writer_thread2 = kthread_run(test_insert_and_search, NULL, "test_insert_and_search");
+	writer_thread3 = kthread_run(test_insert_and_search, NULL, "test_insert_and_search");
+	writer_thread4 = kthread_run(test_insert_and_search, NULL, "test_insert_and_search");
 	printk("hello module! \n");
 	return 0;
 }
@@ -72,43 +60,39 @@ int test_insert_and_search(void* data)
 	
 
 	// search 
-	start_search = ktime_get();
-
+	
 	int i = 0;
 	for (i = 0; i < COUNT; i++){
-		list_for_each_entry_safe(current_node,tmp, &my_list, list ){
+		gclist_for_each_entry_safe(current_node,tmp, &my_list, list ){
 			if(current_node->data == i){
 				
 				struct my_node *new = kmalloc(sizeof(struct my_node), GFP_KERNEL);
 				new->data = i*1000;
-				list_add(&new->list, &(current_node->list));
+				gclist_add(&new->list, &(current_node->list));
 			}
 		}
 	}	
+	ssleep(1);
 
-	end_search = ktime_get();
-
-	printk("search : %lld \n", end_search-start_search);
 	// 잘 삽입되었는지 처음부터 확인
 	printk("================result=================\n");
-	list_for_each_entry_safe(current_node, tmp, &my_list, list){
+	gclist_for_each_entry_safe(current_node, tmp, &my_list, list){
 		printk("current node-> data : %d \n", current_node->data);
+	
+
+	
 	}
 	return 0;
 }
 
-int test_delete(void* data){
-
-	start_delete = ktime_get();
+void test_delete(void){
+	
 	list_for_each_entry_safe(current_node, tmp, &my_list, list){
 		if (current_node->data == 2){
-			printk("delete node %d \n", current_node->data);
+			printk("current node value :%d \n", current_node->data);
 			list_del(&current_node->list);
 			kfree(current_node);
 		}
 	}
-	end_delete = ktime_get();
-
-	printk("delete : %lld \n",end_delete-start_delete);
-	return 0;
+	
 }
